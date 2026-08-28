417a639de856161f037aaf45ccf1ce3d0bf20d61
YanickBu <yanickbu@users.noreply.github.com>
2026-08-28 09:24:28 +0200

    Remove test/scratch files

--- patch-sw.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('sw.js', 'utf8');
-code = code.replace(/const CACHE_NAME = 'split-v14';/, "const CACHE_NAME = 'split-v15';");
-fs.writeFileSync('sw.js', code);
